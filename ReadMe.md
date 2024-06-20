adc5c20936f333da38c9f755454ead9e3fbab547
Sande_Santhosh <103630202+sande88@users.noreply.github.com>
2024-06-20 20:14:22 +0530

    first commit

--- ReadMe.md
+++
@@ -1,3 +0,0 @@
-Webpage Link  https://sande88.github.io/Replica_of_weather_page/
-
-Optimized Only for Computers
--- components/toggle/toggle.js
+++
@@ -1,6 +0,0 @@
-
-let toggle = document.querySelector(".toggle");
-
-function Animetoggle(){
-    toggle.classList.toggle("active");
-}
